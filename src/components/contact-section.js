8899682b00a0b7e8231a3bfd0843ec03e803da06
Janne Peippo <janne.peippo@poutapilvi.fi>
2019-07-21 12:09:46 +0300

    Add contact form

--- src/components/contact-section.js
+++ src/components/contact-section.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import Button from "../components/button"
 import { rhythm } from "../utils/typography"
 
 function ContactSection({ content, locale }) {
@@ -10,12 +11,39 @@ function ContactSection({ content, locale }) {
           if (node.frontmatter.locale !== locale) return false
 
           return (
-            <div key={node.id}>
-              <h2 style={{ marginTop: rhythm(1) }}>
-                {node.frontmatter.sectionHeading}
-              </h2>
-              <div dangerouslySetInnerHTML={{ __html: node.html }} />
-            </div>
+            <Row key={node.id}>
+              <Col>
+                <h2 style={{ marginTop: rhythm(1) }}>
+                  {node.frontmatter.sectionHeading}
+                </h2>
+                <div dangerouslySetInnerHTML={{ __html: node.html }} />
+              </Col>
+              <Col>
+                <form
+                  name="contact"
+                  method="POST"
+                  data-netlify="true"
+                  style={{ marginBottom: 0 }}
+                >
+                  <p>
+                    <label>
+                      Name: <input type="text" name="name" />
+                    </label>
+                  </p>
+                  <p>
+                    <label>
+                      E-mail: <input type="email" name="email" />
+                    </label>
+                  </p>
+                  <p>
+                    <label>
+                      Message: <textarea name="message"></textarea>
+                    </label>
+                  </p>
+                  <Button type="submit">Send</Button>
+                </form>
+              </Col>
+            </Row>
           )
         })}
       </Container>
@@ -32,6 +60,22 @@ const Container = styled.section`
   margin-right: auto;
   max-width: ${rhythm(24)};
   padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
+`
+
+const Row = styled.div`
+  display: flex;
+  align-items: flex-start;
+`
+
+const Col = styled.div`
+  &:last-child {
+    padding: ${rhythm(0.75)};
+    margin-top: ${rhythm(-2)};
+    margin-left: ${rhythm(1)};
+    background-color: white;
+    box-shadow: 0 30px 50px -20px rgba(0, 0, 0, 0.15);
+    border-radius: 0 50px 0 0;
+  }
 `
 
 export default ContactSection
